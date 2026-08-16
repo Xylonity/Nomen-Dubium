18def90ed7e2bd57471b962200cf2cbd0b2c3cb8
xylonity <42015327+Xylonity@users.noreply.github.com>
2026-08-16 22:46:45 +0200

    🌀 Dummy class for the raw fossil descriptor

---
+++ common/src/main/java/dev/xylonity/nomendubium/common/item/fossil/RawFossilItem.java
@@ -0,0 +1,14 @@
+package dev.xylonity.nomendubium.common.item.fossil;
+
+import net.minecraft.world.item.Item;
+
+/**
+ * Generic descriptor whose derived fossil is computed once it's placed in the geologist table
+ */
+public class RawFossilItem extends Item {
+
+    public RawFossilItem(Properties properties) {
+        super(properties);
+    }
+
+}
---
+++ common/src/main/java/dev/xylonity/nomendubium/registry/NomenDubiumItems.java
@@ -0,0 +1,33 @@
+package dev.xylonity.nomendubium.registry;
+
+import dev.xylonity.nomendubium.NomenDubium;
+import dev.xylonity.nomendubium.common.item.fossil.RawFossilItem;
+import net.minecraft.resources.ResourceKey;
+import net.minecraft.world.item.BlockItem;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.level.block.Block;
+
+import java.util.function.Supplier;
+
+public final class NomenDubiumItems {
+
+    public static void init() {
+        ;;
+    }
+
+    public static final Supplier<BlockItem> GEOLOGIST_TABLE = NomenDubium.PLATFORM.registerItem("geologist_table", key -> createBlockItem(key, NomenDubiumBlocks.GEOLOGIST_TABLE.get()));
+    public static final Supplier<Item> RAW_FOSSIL = NomenDubium.PLATFORM.registerItem("raw_fossil", key -> new RawFossilItem(new Item.Properties().stacksTo(1).setId(key)));
+    public static final Supplier<Item> FOSSIL = NomenDubium.PLATFORM.registerItem("fossil", key -> new Item(new Item.Properties().stacksTo(1).setId(key)));
+
+    private static BlockItem createBlockItem(ResourceKey<Item> key, Block block) {
+        final BlockItem item = new BlockItem(
+            block,
+            new Item.Properties().useBlockDescriptionPrefix().setId(key)
+        );
+
+        item.registerBlocks(Item.BY_BLOCK, item);
+
+        return item;
+    }
+
+}
