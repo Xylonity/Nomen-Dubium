6eeda5db229ee13ebb661c68ffc18f8c1cace357
xylonity <42015327+Xylonity@users.noreply.github.com>
2026-08-19 00:11:09 +0200

    ➰ Additional helpers for game state detection (fail, win, begin) for the geo table menu

--- common/src/main/java/dev/xylonity/nomendubium/common/menu/GeologistTableMenu.java
+++ common/src/main/java/dev/xylonity/nomendubium/common/menu/GeologistTableMenu.java
@@ -1,6 +1,7 @@
 package dev.xylonity.nomendubium.common.menu;
 
 import dev.xylonity.nomendubium.common.item.fossil.util.FossilCategory;
+import dev.xylonity.nomendubium.registry.NomenDubiumDataComponents;
 import dev.xylonity.nomendubium.registry.NomenDubiumItems;
 import dev.xylonity.nomendubium.registry.NomenDubiumMenus;
 import net.minecraft.world.Container;
@@ -13,6 +14,8 @@ import net.minecraft.world.inventory.DataSlot;
 import net.minecraft.world.inventory.Slot;
 import net.minecraft.world.item.ItemStack;
 import org.jspecify.annotations.NonNull;
+
+import java.util.Arrays;
 
 public class GeologistTableMenu extends AbstractContainerMenu {
 
@@ -122,14 +125,62 @@ public class GeologistTableMenu extends AbstractContainerMenu {
         this.gameData[index] = value;
     }
 
-    //@Override
-    //public void broadcastChanges() {
-    //    if (!this.player.level().isClientSide()) {
-    //        this.tick();
-    //    }
+    @Override
+    public void broadcastChanges() {
+        if (!this.player.level().isClientSide()) {
+            this.tick();
+        }
 
-    //    super.broadcastChanges();
-    //}
+        super.broadcastChanges();
+    }
+
+    private void tick() {
+        final long tick = this.player.level().getGameTime();
+        if (tick == this.lastGameTick) {
+            return;
+        }
+
+        this.lastGameTick = tick;
+
+        // Checks if the workpiece (the only slot on the table) is the correct item (encased fossil)
+        final ItemStack workpiece = this.container.getItem(TABLE_SLOT);
+        final boolean hasEncasedFossil = workpiece.is(NomenDubiumItems.ENCASED_FOSSIL.get());
+
+        if (!hasEncasedFossil) {
+            // TODO: fail detection
+            return;
+        }
+
+        final FossilCategory category = this.getOrAssignCategory(workpiece);
+        this.set(DATA_FOSSIL_CATEGORY, category.ordinal());
+
+        if (this.getGameState() == STATE_IDLE || this.getGameState() == STATE_LOST) {
+            this.startCountdown();
+            return;
+        }
+        if (this.getGameState() == STATE_COUNTDOWN) {
+            this.set(DATA_COUNTDOWN_REMAINING, this.get(DATA_COUNTDOWN_REMAINING) - 1);
+            if (this.get(DATA_COUNTDOWN_REMAINING) <= 0) {
+                this.startGame();
+            }
+            return;
+        }
+        if (this.getGameState() != STATE_PLAYING) {
+            return;
+        }
+
+        this.set(DATA_GLOBAL_REMAINING, this.get(DATA_GLOBAL_REMAINING) - 1);
+        if (this.getHeldTool() == this.getTool()) {
+            this.set(DATA_ROUND_REMAINING, this.get(DATA_ROUND_REMAINING) - 1);
+        }
+
+        if (this.get(DATA_GLOBAL_REMAINING) <= 0) {
+            this.set(DATA_GLOBAL_REMAINING, 0);
+            this.failGame();
+        } else if (this.get(DATA_ROUND_REMAINING) <= 0) {
+            this.startNextRound();
+        }
+    }
 
     public int getGameState() {
         return this.get(DATA_STATE);
@@ -174,5 +225,73 @@ public class GeologistTableMenu extends AbstractContainerMenu {
     public boolean hasWorkpiece() {
         return !this.container.getItem(TABLE_SLOT).isEmpty();
     }
+
+    private FossilCategory getOrAssignCategory(ItemStack workpiece) {
+        FossilCategory category = FossilCategory.name(workpiece.get(NomenDubiumDataComponents.FOSSIL_CATEGORY.get()));
+        if (category == null) {
+            category = FossilCategory.random(this.player.level().getRandom());
+            workpiece.set(NomenDubiumDataComponents.FOSSIL_CATEGORY.get(), category.serializedName());
+            this.container.setChanged();
+        }
+
+        return category;
+    }
+
+    // Resets the previous attempt and gives the player a moment to get ready
+    private void startCountdown() {
+        this.set(DATA_STATE, STATE_COUNTDOWN);
+        this.set(DATA_COUNTDOWN_REMAINING, COUNTDOWN_DURATION);
+        this.set(DATA_GLOBAL_REMAINING, GLOBAL_DURATION);
+        this.set(DATA_PROGRESS, 0);
+        this.set(DATA_ROUND_INDEX, 0);
+        this.set(DATA_HELD_TOOL, -1);
+    }
+
+    // Starts the first round with a random tool once the countdown is over
+    private void startGame() {
+        this.set(DATA_STATE, STATE_PLAYING);
+        // Specifies a random tool among the 3 available
+        this.set(DATA_TOOL, this.player.level().getRandom().nextInt(3));
+        this.set(DATA_COUNTDOWN_REMAINING, 0);
+        this.beginRound(INITIAL_ROUND_DURATION);
+    }
+
+    // Changes to a different tool and gives the player less time on each new round
+    private void startNextRound() {
+        final int oldTool = this.get(DATA_TOOL);
+        final int offset = 1 + this.player.level().getRandom().nextInt(2);
+
+        this.set(DATA_TOOL, (oldTool + offset) % 3);
+        this.set(DATA_ROUND_INDEX, this.get(DATA_ROUND_INDEX) + 1);
+
+        final int duration = Math.max(MIN_ROUND_DURATION, INITIAL_ROUND_DURATION - this.get(DATA_ROUND_INDEX) * 4);
+        this.beginRound(duration);
+    }
+
+    // Gives early rounds some extra time, then reduces that help as the global timer runs out
+    private void beginRound(int duration) {
+        final int baseDuration = this.getTool() == TOOL_CHISEL ? duration + CHISEL_ROUND_TIME_BONUS : duration;
+        final int remainingDuration = Math.max(0, Math.min(GLOBAL_DURATION, this.get(DATA_GLOBAL_REMAINING)));
+        final int dynamicDuration = baseDuration + Math.round(baseDuration * remainingDuration / (float) GLOBAL_DURATION);
+        final int adjustedDuration = Math.max(1, Math.round(dynamicDuration * ROUND_DURATION_SCALE));
+
+        this.set(DATA_ROUND_DURATION, adjustedDuration);
+        this.set(DATA_ROUND_REMAINING, adjustedDuration);
+
+        this.actionsThisRound = 0;
+        this.lastActionTick = Long.MIN_VALUE;
+    }
+
+    // Clears the current attempt so the table is ready for another encased fossil
+    private void resetGame() {
+        Arrays.fill(this.gameData, 0);
+
+        this.set(DATA_HELD_TOOL, -1);
+        this.set(DATA_FOSSIL_CATEGORY, -1);
+
+        this.actionsThisRound = 0;
+        this.lastActionTick = Long.MIN_VALUE;
+    }
+
 
 }
