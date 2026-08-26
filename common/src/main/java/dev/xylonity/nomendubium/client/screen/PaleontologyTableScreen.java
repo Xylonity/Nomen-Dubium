014aa2bfe78a2b7e3bff371830132231b2e9b84e
xylonity <42015327+Xylonity@users.noreply.github.com>
2026-08-26 22:59:36 +0200

    🆕 Some texture RLs to the table screen

--- common/src/main/java/dev/xylonity/nomendubium/client/screen/PaleontologyTableScreen.java
+++ common/src/main/java/dev/xylonity/nomendubium/client/screen/PaleontologyTableScreen.java
@@ -1,11 +1,24 @@
 package dev.xylonity.nomendubium.client.screen;
 
+import dev.xylonity.nomendubium.NomenDubium;
 import dev.xylonity.nomendubium.common.menu.PaleontologyTableMenu;
 import net.minecraft.client.gui.screens.inventory.AbstractContainerScreen;
 import net.minecraft.network.chat.Component;
+import net.minecraft.resources.Identifier;
 import net.minecraft.world.entity.player.Inventory;
 
 public class PaleontologyTableScreen extends AbstractContainerScreen<PaleontologyTableMenu> {
+
+    private static final Identifier BACKGROUND = NomenDubium.of("textures/gui/paleontology_table.png");
+    private static final Identifier CHISEL = NomenDubium.of("textures/gui/chisel.png");
+    private static final Identifier HAMMER = NomenDubium.of("textures/gui/hammer.png");
+    private static final Identifier BRUSH = NomenDubium.of("textures/gui/brush.png");
+    private static final Identifier[] FOSSIL_TEXTURES = {
+            NomenDubium.of("textures/gui/fossil_1.png"),
+            NomenDubium.of("textures/gui/fossil_2.png"),
+            NomenDubium.of("textures/gui/fossil_3.png"),
+            NomenDubium.of("textures/gui/fossil_4.png")
+    };
 
     private static final int GUI_WIDTH = 256;
     private static final int GUI_HEIGHT = 210;
