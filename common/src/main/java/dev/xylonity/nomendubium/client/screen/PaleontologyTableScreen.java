75bd253d8c113edc685994f0b017ebdbed24d47f
xylonity <42015327+Xylonity@users.noreply.github.com>
2026-08-26 22:58:29 +0200

    🐝 Added some conventional data to the table screen

--- common/src/main/java/dev/xylonity/nomendubium/client/screen/PaleontologyTableScreen.java
+++ common/src/main/java/dev/xylonity/nomendubium/client/screen/PaleontologyTableScreen.java
@@ -7,6 +7,19 @@ import net.minecraft.world.entity.player.Inventory;
 
 public class PaleontologyTableScreen extends AbstractContainerScreen<PaleontologyTableMenu> {
 
+    private static final int GUI_WIDTH = 256;
+    private static final int GUI_HEIGHT = 210;
+
+    private static final int FOSSIL_X = 96;
+    private static final int FOSSIL_Y = 19;
+    private static final int FOSSIL_SIZE = 64;
+    private static final int FOSSIL_TEXTURE_SIZE = 80;
+    private static final int FOSSIL_POP_DURATION = 10;
+
+    private static final int TOOL_X = 11;
+    private static final int TOOL_WIDTH = 32;
+    private static final int TOOL_HEIGHT = 34;
+
     public PaleontologyTableScreen(PaleontologyTableMenu menu, Inventory inventory, Component title) {
         super(menu, inventory, title);
     }
