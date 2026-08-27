d0238ea9980521e86a1cad2b674d277162c13622
xylonity <42015327+Xylonity@users.noreply.github.com>
2026-08-27 17:01:33 +0200

    🙅 Second creative tab for the chimera skeleton parts

--- common/src/main/java/dev/xylonity/nomendubium/registry/NomenDubiumCreativeTabs.java
+++ common/src/main/java/dev/xylonity/nomendubium/registry/NomenDubiumCreativeTabs.java
@@ -14,9 +14,9 @@ public final class NomenDubiumCreativeTabs {
         ;;
     }
 
-    public static final Supplier<CreativeModeTab> CREATIVE_TAB = NomenDubium.PLATFORM.registerCreativeTab(
+    public static final Supplier<CreativeModeTab> MAIN_CREATIVE_TAB = NomenDubium.PLATFORM.registerCreativeTab(
         "nomendubiumtab",
-        Component.translatable("creativeTab.nomendubium.tab"),
+        Component.translatable("creativetab.nomendubium.title"),
         () -> new ItemStack(NomenDubiumItems.ENCASED_FOSSIL.get()),
         List.of(
             () -> new ItemStack(NomenDubiumItems.PALEONTOLOGY_TABLE.get()),
@@ -48,28 +48,37 @@ public final class NomenDubiumCreativeTabs {
             () -> new ItemStack(NomenDubiumItems.PRIMITIVE_ARROW.get()),
             () -> new ItemStack(NomenDubiumItems.PREHISTORIC_MAW.get()),
             () -> new ItemStack(NomenDubiumItems.FOSSILISED_MAW.get()),
-            () -> new ItemStack(NomenDubiumItems.REGENERATING_CHOP.get()),
-            () -> NomenDubiumItems.FOSSIL.get().createStack("hulking_body"),
-            () -> NomenDubiumItems.FOSSIL.get().createStack("shelled_body"),
-            () -> NomenDubiumItems.FOSSIL.get().createStack("avian_body"),
-            () -> NomenDubiumItems.FOSSIL.get().createStack("lanky_body"),
-            () -> NomenDubiumItems.FOSSIL.get().createStack("puffy_body"),
-            () -> NomenDubiumItems.FOSSIL.get().createStack("crunching_head"),
-            () -> NomenDubiumItems.FOSSIL.get().createStack("shielded_head"),
-            () -> NomenDubiumItems.FOSSIL.get().createStack("snarled_head"),
-            () -> NomenDubiumItems.FOSSIL.get().createStack("beaked_head"),
-            () -> NomenDubiumItems.FOSSIL.get().createStack("snorting_head"),
-            () -> NomenDubiumItems.FOSSIL.get().createStack("spiked_tail"),
-            () -> NomenDubiumItems.FOSSIL.get().createStack("stubby_tail"),
-            () -> NomenDubiumItems.FOSSIL.get().createStack("clubbed_tail"),
-            () -> NomenDubiumItems.FOSSIL.get().createStack("fan_tail"),
-            () -> NomenDubiumItems.FOSSIL.get().createStack("speared_tail"),
-            () -> NomenDubiumItems.FOSSIL.get().createStack("boney_plates_back"),
-            () -> NomenDubiumItems.FOSSIL.get().createStack("dorsal_scales_back"),
-            () -> NomenDubiumItems.FOSSIL.get().createStack("spikes_back"),
-            () -> NomenDubiumItems.FOSSIL.get().createStack("spine_sail_back"),
-            () -> NomenDubiumItems.FOSSIL.get().createStack("thorns_back")
+            () -> new ItemStack(NomenDubiumItems.REGENERATING_CHOP.get())
         )
+
+    );
+
+    public static final Supplier<CreativeModeTab> CHIMERA_CREATIVE_TAB = NomenDubium.PLATFORM.registerCreativeTab(
+            "nomendubiumchimeratab",
+            Component.translatable("creativetab.nomendubium.chimera.title"),
+            () -> NomenDubiumItems.FOSSIL.get().createStack("shielded_head"),
+            List.of(
+                    () -> NomenDubiumItems.FOSSIL.get().createStack("hulking_body"),
+                    () -> NomenDubiumItems.FOSSIL.get().createStack("shelled_body"),
+                    () -> NomenDubiumItems.FOSSIL.get().createStack("avian_body"),
+                    () -> NomenDubiumItems.FOSSIL.get().createStack("lanky_body"),
+                    () -> NomenDubiumItems.FOSSIL.get().createStack("puffy_body"),
+                    () -> NomenDubiumItems.FOSSIL.get().createStack("crunching_head"),
+                    () -> NomenDubiumItems.FOSSIL.get().createStack("shielded_head"),
+                    () -> NomenDubiumItems.FOSSIL.get().createStack("snarled_head"),
+                    () -> NomenDubiumItems.FOSSIL.get().createStack("beaked_head"),
+                    () -> NomenDubiumItems.FOSSIL.get().createStack("snorting_head"),
+                    () -> NomenDubiumItems.FOSSIL.get().createStack("spiked_tail"),
+                    () -> NomenDubiumItems.FOSSIL.get().createStack("stubby_tail"),
+                    () -> NomenDubiumItems.FOSSIL.get().createStack("clubbed_tail"),
+                    () -> NomenDubiumItems.FOSSIL.get().createStack("fan_tail"),
+                    () -> NomenDubiumItems.FOSSIL.get().createStack("speared_tail"),
+                    () -> NomenDubiumItems.FOSSIL.get().createStack("boney_plates_back"),
+                    () -> NomenDubiumItems.FOSSIL.get().createStack("dorsal_scales_back"),
+                    () -> NomenDubiumItems.FOSSIL.get().createStack("spikes_back"),
+                    () -> NomenDubiumItems.FOSSIL.get().createStack("spine_sail_back"),
+                    () -> NomenDubiumItems.FOSSIL.get().createStack("thorns_back")
+            )
 
     );
 
