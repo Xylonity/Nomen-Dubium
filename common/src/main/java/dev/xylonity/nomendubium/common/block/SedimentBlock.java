f8123b8f8a57274c7ac6250ac6d78e8b30499270
xylonity <42015327+Xylonity@users.noreply.github.com>
2026-08-28 13:41:00 +0200

    ❔ Sediment acting like a falling block

--- common/src/main/java/dev/xylonity/nomendubium/common/block/SedimentBlock.java
+++ common/src/main/java/dev/xylonity/nomendubium/common/block/SedimentBlock.java
@@ -1,8 +1,13 @@
 package dev.xylonity.nomendubium.common.block;
 
 import net.minecraft.core.BlockPos;
+import net.minecraft.core.Direction;
 import net.minecraft.server.level.ServerLevel;
 import net.minecraft.util.RandomSource;
+import net.minecraft.world.entity.item.FallingBlockEntity;
+import net.minecraft.world.level.LevelReader;
+import net.minecraft.world.level.ScheduledTickAccess;
+import net.minecraft.world.level.block.FallingBlock;
 import net.minecraft.world.level.block.SnowLayerBlock;
 import net.minecraft.world.level.block.state.BlockState;
 
@@ -13,7 +18,22 @@ public final class SedimentBlock extends SnowLayerBlock {
     }
 
     @Override
+    protected BlockState updateShape(BlockState state, LevelReader level, ScheduledTickAccess tickAccess, BlockPos pos, Direction direction, BlockPos neighborPos, BlockState neighborState, RandomSource random) {
+        tickAccess.scheduleTick(pos, this, 2);
+        return state;
+    }
+
+    @Override
+    protected void tick(BlockState state, ServerLevel level, BlockPos pos, RandomSource random) {
+        if (FallingBlock.isFree(level.getBlockState(pos.below())) && pos.getY() >= level.getMinY()) {
+            FallingBlockEntity.fall(level, pos, state);
+        }
+
+    }
+
+    @Override
     protected void randomTick(BlockState state, ServerLevel level, BlockPos pos, RandomSource random) {
+        ;;
     }
 
 }
