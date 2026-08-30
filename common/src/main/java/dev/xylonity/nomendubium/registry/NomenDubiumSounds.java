f6ff1659dca080e2be2069afaf9a4052bcdc9ff8
xylonity <42015327+Xylonity@users.noreply.github.com>
2026-08-30 19:10:00 +0200

    👹 Some extra sounds

--- common/src/main/java/dev/xylonity/nomendubium/registry/NomenDubiumSounds.java
+++ common/src/main/java/dev/xylonity/nomendubium/registry/NomenDubiumSounds.java
@@ -12,5 +12,8 @@ public final class NomenDubiumSounds {
     }
 
     public static final Supplier<SoundEvent> WHISPERS = NomenDubium.PLATFORM.registerSound("whispers");
+    public static final Supplier<SoundEvent> CHIMERA_IDLE = NomenDubium.PLATFORM.registerSound("chimera_idle");
+    public static final Supplier<SoundEvent> CHIMERA_DEATH = NomenDubium.PLATFORM.registerSound("chimera_death");
+    public static final Supplier<SoundEvent> CHIMERA_ROAR = NomenDubium.PLATFORM.registerSound("chimera_roar");
 
 }
